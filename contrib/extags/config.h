fcb934817de31680f5174c850abb253a9e965d9f
adamy <adamy@git.code.sf.net>
2018-10-20 00:16:09 +0100

    _MSC_VER build options

---
+++ contrib/extags/config.h
@@ -0,0 +1,27 @@
+#ifndef CONFIG_H_INCLUDED
+#define CONFIG_H_INCLUDED
+/*
+ *  extags <config.h>
+ */
+
+#include "../contrib_config.h"
+
+#if defined(_MSC_VER)
+# define MANUAL_GLOBBING 1
+# ifndef HAVE_IO_H
+# define HAVE_IO_H
+# endif
+
+# if !defined(HAVE_FINDFIRST) && !defined(HAVE__FINDFIRST)
+#  define HAVE__FINDFIRST
+# endif
+# if _MSC_VER >= 1300
+#  define findfirst_t intptr_t                  /* Visual Studio 7 */
+# else                                     
+#  define findfirst_t long                      /* Visual Studio 6 or earlier */
+# endif
+#endif
+#define tempnam(dir,pfx) _tempnam(dir,pfx)
+#define TMPDIR "\\"
+
+#endif  /*CONFIG_H_INCLUDED*/
