80753d38ae1b71ca8b70eecb06ddedebc3f04d99
adamy <adamy@git.code.sf.net>
2019-05-21 21:19:16 +0100

    strstr and strcasecmp definitions

--- contrib/extags/config.h
+++ contrib/extags/config.h
@@ -8,8 +8,16 @@
 
 #if defined(_MSC_VER)
 # define MANUAL_GLOBBING 1
+
 # ifndef HAVE_IO_H
 # define HAVE_IO_H
+# endif
+
+# ifndef HAVE_STRSTR
+# define HAVE_STRSTR
+# endif
+# ifndef HAVE_STRCASECMP
+# define HAVE_STRCASECMP
 # endif
 
 # if !defined(HAVE_FINDFIRST) && !defined(HAVE__FINDFIRST)
@@ -17,10 +25,11 @@
 # endif
 # if _MSC_VER >= 1300
 #  define findfirst_t intptr_t                  /* Visual Studio 7 */
-# else                                     
+# else
 #  define findfirst_t long                      /* Visual Studio 6 or earlier */
 # endif
-#endif
+#endif  /*MSC_VER*/
+
 #define tempnam(dir,pfx) _tempnam(dir,pfx)
 #define TMPDIR "\\"
 
