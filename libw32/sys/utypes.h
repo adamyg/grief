cf26c460208bd9f553492d021a7404fd65232bf1
adamy <adamy@git.code.sf.net>
2021-06-10 22:35:53 +0800

    msvc 16.9

--- libw32/sys/utypes.h
+++ libw32/sys/utypes.h
@@ -1,7 +1,7 @@
 #ifndef LIBW32_SYS_UTYPES_H_INCLUDED
 #define LIBW32_SYS_UTYPES_H_INCLUDED
 #include <edidentifier.h>
-__CIDENT_RCSID(gr_libw32_sys_utypes_h,"$Id: utypes.h,v 1.28 2021/04/07 16:15:41 cvsuser Exp $")
+__CIDENT_RCSID(gr_libw32_sys_utypes_h,"$Id: utypes.h,v 1.29 2021/06/10 14:29:08 cvsuser Exp $")
 __CPRAGMA_ONCE
 
 /* -*- mode: c; indent-width: 4; -*- */
@@ -30,7 +30,7 @@ __CPRAGMA_ONCE
 #if (_MSC_VER != 1600)                          /* MSVC 10/2010 */
 #if (_MSC_VER != 1900)                          /* MSVC 19/2015 */
 #if (_MSC_VER <  1910 || _MSC_VER > 1916)       /* MSVC 19.10 .. 16/2017 */
-#if (_MSC_VER > 1928)                           /* MSVC 19.20 / 2019.08 */
+#if (_MSC_VER > 1929)                           /* MSVC 19.20 / 2019.09 */
 #error utypes.h: untested MSVC Version (2005 -- 2019.08) only ...
  //see: https://en.wikipedia.org/wiki/Microsoft_Visual_C%2B%2B
 #endif //2019
--- libw32/unistd.h
+++ libw32/unistd.h
@@ -1,7 +1,7 @@
 #ifndef LIBW32_UNISTD_H_INCLUDED
 #define LIBW32_UNISTD_H_INCLUDED
 #include <edidentifier.h>
-__CIDENT_RCSID(gr_libw32_unistd_h,"$Id: unistd.h,v 1.52 2021/06/10 06:13:03 cvsuser Exp $")
+__CIDENT_RCSID(gr_libw32_unistd_h,"$Id: unistd.h,v 1.53 2021/06/10 14:29:08 cvsuser Exp $")
 __CPRAGMA_ONCE
 
 /* -*- mode: c; indent-width: 4; -*- */
@@ -39,7 +39,7 @@ __CPRAGMA_ONCE
 #if (_MSC_VER != 1600)                          /* MSVC 10/2010 */
 #if (_MSC_VER != 1900)                          /* MSVC 19/2015 */
 #if (_MSC_VER <  1910 || _MSC_VER > 1916)       /* MSVC 19.10 .. 16/2017 */
-#if (_MSC_VER > 1928)                           /* MSVC 19.20 / 2019.08 */
+#if (_MSC_VER > 1929)                           /* MSVC 19.20 / 2019.09 */
 #error unistd.h: untested MSVC Version (2005 -- 2019.08) only ...
 	//see: https://en.wikipedia.org/wiki/Microsoft_Visual_C%2B%2B
 #endif //2019
