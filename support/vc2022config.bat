e1a957e20f5fed49048d6984044395432f0a2d9d
adamyg <adamyg@gmail.com>
2022-03-22 00:20:58 +0800

    ci: config

--- support/vc2022config.bat
+++ support/vc2022config.bat
@@ -1,6 +1,6 @@
 @echo off
 rem
-rem Microsoft Visual Studio C/C++ 2019+
+rem Microsoft Visual Studio C/C++ 2022+
 rem
 if not defined GNUWIN32 (
         set GNUWIN32=\devl\gnuwin32
@@ -8,5 +8,5 @@ if not defined GNUWIN32 (
 if not defined PERL (
         set PERL=perl
 )
-%PERL% makelib.pl --gnuwin32=%GNUWIN32% --icu=auto vc2019 %1 %2 %3 %4
+%PERL% makelib.pl --gnuwin32=%GNUWIN32% --icu=auto vc2022 %1 %2 %3 %4
 
