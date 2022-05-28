ce7030e7441e5fa20901c5bfd9ae066a4794fd6d
adamyg <adamyg@gmail.com>
2022-05-28 18:32:49 +0800

    gcc warnings

--- util/grkeytest.c
+++ util/grkeytest.c
@@ -7,6 +7,16 @@
 #endif
 
 #if defined(_WIN32)
+
+#if !defined(_WIN32_WINNT)
+#undef _WIN32_WINNT
+#define _WIN32_WINNT 0x601
+#elif (_WIN32_WINNT < 0x601)
+#undef _WIN32_WINNT
+#define _WIN32_WINNT 0x601
+#endif
+#undef WINVER
+#define WINVER _WIN32_WINNT
 #include "w32keytest.c"
 
 #else
