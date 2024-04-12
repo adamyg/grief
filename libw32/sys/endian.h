59652acb28fdea2a1dd86c00c5171f8be3163d04
adamyg <adamyg@gmail.com>
2024-04-12 10:35:11 +0800

    x64 detection

--- libw32/sys/endian.h
+++ libw32/sys/endian.h
@@ -1,7 +1,7 @@
 #ifndef GR_ENDIAN_H_INCLUDED
 #define GR_ENDIAN_H_INCLUDED
 #include <edidentifier.h>
-__CIDENT_RCSID(gr_libw32_sys_endian_h,"$Id: endian.h,v 1.12 2024/01/01 12:37:09 cvsuser Exp $")
+__CIDENT_RCSID(gr_libw32_sys_endian_h,"$Id: endian.h,v 1.13 2024/04/12 02:33:56 cvsuser Exp $")
 __CPRAGMA_ONCE
 
 /* -*- mode: c; indent-width: 4; -*-
@@ -39,7 +39,9 @@ __CPRAGMA_ONCE
 #elif defined(IS_BIG_ENDIAN)
 #define __BYTE_ORDER        _BIG_ENDIAN
 #else
-#   if defined(_M_IX86)
+#   if defined(_M_IA64) || defined(_M_AMD64) // 64-bit Intel/AMD
+#       define IS_LITTLE_ENDIAN 1
+#   elif defined(_M_IX86)   // 32-bit Intel
 #       define IS_LITTLE_ENDIAN 1
 #   elif defined(_M_MRX000)
 #       define IS_LITTLE_ENDIAN 1
