4e136b8a277a8d9d25245dce65cfca80f87a466c
adamyg <adamyg@gmail.com>
2024-09-09 19:39:59 +0800

    msvc 19.41

--- libw32/unistd.h
+++ libw32/unistd.h
@@ -1,7 +1,7 @@
 #ifndef LIBW32_UNISTD_H_INCLUDED
 #define LIBW32_UNISTD_H_INCLUDED
 #include <edidentifier.h>
-__CIDENT_RCSID(gr_libw32_unistd_h,"$Id: unistd.h,v 1.73 2024/06/09 14:05:43 cvsuser Exp $")
+__CIDENT_RCSID(gr_libw32_unistd_h,"$Id: unistd.h,v 1.74 2024/09/09 11:39:14 cvsuser Exp $")
 __CPRAGMA_ONCE
 
 /* -*- mode: c; indent-width: 4; -*- */
@@ -40,8 +40,8 @@ __CPRAGMA_ONCE
 #if (_MSC_VER != 1900)                          /* MSVC 19/2015 */
 #if (_MSC_VER <  1910 || _MSC_VER > 1916)       /* MSVC 2017: 19.10 .. 16 */
 #if (_MSC_VER > 1929)                           /* MSVC 2019: 19.20 .. 29 */
-#if (_MSC_VER > 1940)                           /* MSVC 2022: 19.30 .. 40 */
-#error unistd.h: untested MSVC Version (2005 -- 2022 19.40)
+#if (_MSC_VER > 1941)                           /* MSVC 2022: 19.30 .. 41 */
+#error unistd.h: untested MSVC Version (2005 -- 2022 19.41)
 	//see: https://en.wikipedia.org/wiki/Microsoft_Visual_C%2B%2B
 #endif //2022
 #endif //2019
